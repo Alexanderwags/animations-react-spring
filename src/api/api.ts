ff70b0e07b1453124763202ff299a032b61055fe
william_ws7 🔥 <alexander171296@gmail.com>
2021-02-03 19:51:09 -0400

    style: :package: new packages

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,3 @@
-export const key = "521371c69a3d4c06bef5564bffd0d160"
+export const ApiKey = "521371c69a3d4c06bef5564bffd0d160"
+export const TopHeadlines = "/v2/top-headlines"
+export const Everything = "/v2/everything"
